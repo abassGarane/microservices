8869b5b12109622119e7ef432dee484c2e9f5f96
abassGarane <thelibertinewreck@gmail.com>
2022-09-11 08:48:18 +0300

    Created a simple mux and handler for requests

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -13,8 +15,16 @@ func main()  {
 
   // map urls to routes
   mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    d,err := ioutil.ReadAll(r.Body)
+    if err != nil{
+    //   w.WriteHeader(http.StatusBadRequest)
+    //   w.Write([]byte("Oops! Could not parse request !"))
+      http.Error(w, "Could not parse request", http.StatusBadRequest)
+      return
+    }
     log.Println("Hello from page")
+    fmt.Fprintf(w,"Response is %s\n", d)
   })
 
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":8080", mux)
 }

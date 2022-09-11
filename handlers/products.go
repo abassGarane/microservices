75eb702151386f92ebf68f31f402d4986c60cf6c
abassGarane <thelibertinewreck@gmail.com>
2022-09-11 13:36:53 +0300

    Product handler init

---
+++ handlers/products.go
@@ -0,0 +1,38 @@
+package handlers
+
+import "time"
+
+type Product struct{
+  ID int
+  Name string
+  Description string
+  Price float32
+  SKU string
+  CreatedAt string
+  UpdatedAt string
+  DeletedAt string
+}
+
+var productList = []*Product{
+  {
+    ID:1,
+    Name:"Rice",
+    Description: "A bag of rice",
+    Price:2200,
+    SKU: "cdcd34",
+    CreatedAt: time.Now().UTC().String(),
+    UpdatedAt: time.Now().UTC().String(),
+    DeletedAt: time.Now().UTC().String(),
+  },
+ {
+    ID:2,
+    Name:"Beef",
+    Description: "A real pound of beef",
+    Price:800.90,
+    SKU: "cdcd66",
+    CreatedAt: time.Now().UTC().String(),
+    UpdatedAt: time.Now().UTC().String(),
+    DeletedAt: time.Now().UTC().String(),
+  },
+
+}

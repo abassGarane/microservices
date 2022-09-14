77b65f869ad36343f7dfb02297a82dcb5970b32d
abassGarane <thelibertinewreck@gmail.com>
2022-09-14 12:16:39 +0300

    Included test files

---
+++ data/product_test.go
@@ -0,0 +1,18 @@
+package data
+
+import "testing"
+
+func TestIsUnderValidation(t *testing.T)  {
+  p := &Product{
+    ID: 3,
+    Price: 33.55,
+    Description: "A wild stuff",
+    Name: "Rice",
+    SKU: "DRCED454",
+  } 
+  err := p.Validate()
+
+  if err != nil{
+    t.Fatal(err)
+  }
+}
